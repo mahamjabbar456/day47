73ebc9a5e6ff34c2237dfb4c364ed70dd968045e
mahamjabbar456 <mahamjabbar320@gmail.com>
2024-04-21 21:49:47 +0500

    i learn about reserved word and await word.

---
+++ index.ts
@@ -0,0 +1,33 @@
+// Question 139
+// Reserved word : let
+let count = 5;
+
+// Reserved word : if
+if(count > 0){
+    console.log("Count is greater than 0.")
+}
+
+// Reserved word : return
+function add(a:number,b:number){
+    return a+b;
+}
+
+// Question 140
+// let if = 5; // This line would cause a syntax error
+
+console.log(
+    "Using a reserved word as a variable name causes a syntax error in JavaScript."
+  );
+  
+// Question 141
+function fetchSomething() {};
+async function fetchData() {
+    // Assuming fetchSomething returns a Promise
+    const data = await fetchSomething();
+    console.log(data);
+    // The 'await' keyword pauses the execution until the Promise settles, and then resumes with the resolved value.
+  }
+  
+  console.log(
+    "The 'await' keyword allows asynchronous, promise-based behavior to be written in a cleaner, more linear fashion."
+  );
